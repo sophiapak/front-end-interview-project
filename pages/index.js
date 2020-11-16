be8612872453179d9468ec8f80ef28d50a62167d
Kyle McDonald <hello@kylemcd.com>
2020-11-16 15:54:47 -0600

    📦 NEW: Update index file

--- pages/index.js
+++ pages/index.js
@@ -8,10 +8,13 @@ const Index = () => {
         <link rel="icon" href="/favicon.ico" />
         <link rel="stylesheet" href="https://use.typekit.net/zdz0zoa.css" />
       </Head>
-
-      <main>hi</main>
-
-      <footer></footer>
+      <main>
+        <h1>Welcome to the Foxtrot front-end coding project</h1>
+        <p>
+          Please take a look at the README before staring and feel free to reach
+          out with any questions or concerns!
+        </p>
+      </main>
     </div>
   );
 };
